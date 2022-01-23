3dff3e5f08220269b7f84e12d40da23bd1bea0de
mehedihassansovon <mehedi15-10712@diu.edu.bd>
2022-01-23 23:14:23 +0600

    boostrap carousel done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 
 import './App.css';
+import {  BrowserRouter as Router,Route } from "react-router-dom";
+import PreNavbar from './components/PerNavbar';
+import Navbar from './components/Navbar.js';
+import Slider from "./components/Slider.js";
+import data from "./data/data.json";
 
 function App() {
   return (
-    <>
-      
-    </>
+    <Router>
+        <PreNavbar/>
+        <Navbar/>
+        <Slider start={data.banner.start} />
+
+    </Router>
   );
 }
 
